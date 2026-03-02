6bcf1a3f4916a36b4a0ef7ec46c0827f4bfc19c8
life <arnas.sokolovas@gmail.com>
2026-03-02 15:47:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 // I am using App.jsx to define my router and to use Layout
 // as the wrapper
 // Here I will show Home, MyPlants and About pages inside the Outlet
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import MyPlants from "./pages/MyPlants";
 import About from "./pages/About";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-    // using index to define the default child route
-      { index: true, element: <Home />},
-      { path: "myplants", element: <MyPlants />},
-      { path: "about", element: <About />}
+      { index: true, element: <Home /> },
+      { path: "myplants", element: <MyPlants /> },
+      { path: "about", element: <About /> },
     ],
   },
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
